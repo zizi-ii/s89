ea42394a2ed435ada32e7fe24a1419d9a62ddd41
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-21 06:04:19 +0900

    two

---
+++ src/s89/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+	public static void main(String[] args) {
+		// 자신의 학번과 이름을 출력하라
+		System.out.println("hello");
+	}
+}
--- src/s89/Main.java
+++ src/s89/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// 자신의 학번과 이름을 출력하라
 		System.out.println("1234567 이지우");
+		System.out.println(Hello.sum(10));
+		System.out.println(Hello.sum(100));
 	}
 }
