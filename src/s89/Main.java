aec780e8dbc13511082865a23354f2df7b391103
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-21 06:14:35 +0900

    six

--- src/s89/Main.java
+++ src/s89/Main.java
@@ -1,13 +1,15 @@
 package s89;
 
+import static java.lang.System.out;
+
 public class Main {
 	public static void main(String[] args) {
 		// 자신의 학번과 이름을 출력하라
-		System.out.println("1234567 이지우");
-		System.out.println(Hello.sum(10));
-		System.out.println(Hello.sum(100));
-		System.out.println(Hello.sum(1000));
-		System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4}));
-		System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6}));
+		out.println("1234567 이지우");
+		out.println(Hello.sum(10));
+		out.println(Hello.sum(100));
+		out.println(Hello.sum(1000));
+		out.println(Sansu.max(new int[] {8, 5, 6, 9, 4}));
+		out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6}));
 	}
 }
